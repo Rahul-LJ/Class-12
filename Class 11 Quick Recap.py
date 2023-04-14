42c90ae03ceb85d8a47f5bd82e7fa0ce62eb83fc
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-04-14 22:32:09 +0530

    Add files via upload

---
+++ Class 11 Quick Recap.py
@@ -0,0 +1,76 @@
+'''
+IDLE Integrated devalopment learning environment
+
+tokens-Punctuators Identifiers Constant Keyword Operators
+
+escape sequence- \n (next line) \a (bell sound) () \t (tab space)
+
+priority ig ** // % / * + -
+
+AND both to be true
+OR either of one be true
+NOT opposites
+
+membership - in, not in
+identity - is, is not
+
+            MUTABLE                  IMMUTABLE
+                |                        |
+      ____________________            _________________________
+      |                  |           |       |       |        |    
+    list            dictionary      int    string  tuple   boolean
+
+while - initialisation, condition, iteration
+range - (start,stop,step)
+
+break - it breaks the loop (comes out of the loop)
+continue - it skips the current condition in a loop
+pass - does nothing
+
+string function
+slicing    - [start,stop,step]
+capitalize - x.capitalise()           caps the first letter of the string
+title      - x.title()                caps every first letter of words
+upper,lower- x.upper()                caps the entire string
+count      - x.count('')              counts the given character
+find       - x.find('',start)         prints the index value of the chr
+split      - x.split('')              splits the string wherever the chr appears
+partition  - x.partition('')          splits 3 parts, 1)before 2)the chr 3)after
+replace    - x.replace(old,new,count)
+
+list function
+eval       - eval('')                 takes one input which is a str, evaluates
+append     - l.append()               1 argument, adds as last element in list
+extend     - l.extend()               1 argument, a variable (for multi values)
+pop        - l.pop()                  default-last element, removes given index
+remove     - l.remove()               1 argument, removes given Value
+clear      - l.clear()                removes all elements, list still exist
+count      - x.count('')              counts the given character
+sort       - x.sort()                 no argument, increasing order
+reverse    - x.reverse()              no argument, decreasing order
+
+Matrix
+basically nested list
+
+|_______columns j
+|
+|
+|
+rows i
+
+1
+4 5      if (i>=j):
+7 8 9
+
+1 2 3
+  5 6    if (i<=j):
+    9
+
+    3
+  5 6    if (i+j>=m-1):
+7 8 7
+
+1 2 3
+4 5      if (i+j<=m-1):
+7
+'''
