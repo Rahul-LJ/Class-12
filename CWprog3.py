82454f5106a0821b9113113903c8cbf3ab4222fb
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-04-27 17:34:16 +0530

    Update CWprog3.py

--- CWprog3.py
+++ CWprog3.py
@@ -1,6 +1,8 @@
-#WAP implementing 2 default arguments and 3 default arguments for 2 UDF si() and ci() and show the function calls with 1,2,3 arguments for si() and 1,2 for cs()
-'''SI= prt/100
-   CI=p+(1+(r/100))^t'''
+'''WAP implementing 2 default arguments and 3 default arguments for 2 UDF si() and ci()
+and show the function calls with 1,2,3 arguments for si() and 1,2 for cs()
+
+SI= prt/100
+CI=p+(1+(r/100))^t'''
 
 def si(p=1000,r=10,t=2):
     print((p*r*t)/100)
