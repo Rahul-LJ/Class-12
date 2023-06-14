b9971164f5d58053f774d6789ff90a0e67cd62cf
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-06-14 22:29:53 +0530

    Add files via upload

---
+++ Series pattern.py
@@ -0,0 +1,19 @@
+def fac(n):
+    s=1
+    for i in range(1,n+1):
+        s*=i
+    return s
+
+
+n,x=map(int,input('Enter value for n and x').split())
+den=2
+s,i=0,1
+power=1
+while n>0:
+    s+=(i*((x**power)/fac(den)))
+    power+=2
+    den+=2
+    n-=1
+    print(s)
+print(s)
+    
