4fca63d1396c08d1d2b4400fbc833f2c17cd229f
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-06-28 19:45:25 +0530

    Add files via upload

---
+++ File Handling (in progress).py
@@ -0,0 +1,23 @@
+#file handling
+'''
+   2 types of file
+     ___________
+    |           |
+text file    binary file
+
+text file:
+
+open('<filename>') - basically opens the given file name
+                     (default opens in read mode)
+<filename>.close() - breaks the connection between fileobject and file
+
+     3 modes
+  _____________
+ |      |      |
+read  write  append
+
+read only (r) is used to read from existing txt file
+
+
+
+'''
