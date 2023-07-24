bde54205e004537d457918b84d89ae395123899a
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-07-24 21:34:38 +0530

    File Handling (in progress).py

--- File Handling (in progress).py
+++ File Handling (in progress).py
@@ -1,4 +1,4 @@
-#file handling
+#FILE HANDLING
 '''
    2 types of file
      ___________
@@ -23,19 +23,40 @@ open('<filename>') - basically opens the given file name
 <filename>.close() - breaks the connection between fileobject and file
 ________________________________________________________________________
 
-           3 modes              file_object = open('<filename>','mode')
-  ________________________
- |      |      |          |
-read  write  append  read & write
+     3 modes              file_object = open('<filename>','mode')
+  _____________
+ |      |      |
+read  write  append
 
-read only (r) is used to read from existing txt file
-write only (w) is used to Create a file by adding string (if file alr exist, it OVERWRITES)
-append only (a) same as write to add data but if file alr exist, it creates a new file
-read and write (r+) when you want to read aswell as write
+read only (r)       is used to read from existing txt file
+write only (w)      is used to Create a file by adding string (if file alr exist, it OVERWRITES)
+append only (a)     same as write to add data but if file alr exist, it creates a new file
+read and write (r+) when you want to first read then write
+write and read (w+) when you want to first write (overwrite) then read it
 ________________________________________________________________________
 
+#remember these func moves the pointer!
+<file_object>.read(n)     - reads the entire file if no argument (reads till n bytes)
 
-<file_object>.read(n)     - reads the entire file (reads till n bytes)
-<file_object>.readline()  - reads 1 word, i.e. reads till a space appears (reads the n)
-<file_object>.readlines() - 
+<file_object>.readline()  - reads till EOL if no argument (reads till n bytes)
+
+<file_object>.readlines() - reads entire file if no argument in a LIST (reads n lines)
+
+<file_object>.write()     - to add data to a file
+                            if file alr exists, it'll overwrite (deletes the old, and writes the new)
+                            {print of this function gives you the No.Of.Characters in write}
+                            {if used in r+ mode, it REPLACES the required No.Of.Chr to the Existing one with the new}
+                            
+<file_object>.writelines()- to add a LIST of data. To add many values.
+                            (eg: l = ['line1','\nline2'] )
+
+<file_object>.seek()      - for 1 argument
+                            move the pointer after the given pos.of character (eg: seek(1) comes b/w 1,2 character)
+
+                            for 2 arguments (ONLY FOR BINARY only)
+                            <file_object>.seek( no of STEPS to move left (-ve) or right (+ve) , FROM )
+                            where FROM can be 0 - beghinning of the file
+                                              1 - current position
+                                              2 - end of the file
+
 '''
