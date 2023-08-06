52f20404bf4c64b24131e255e753e311589e06d3
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-08-06 11:29:38 +0530

    Add files via upload

---
+++ Record Codes/3 Menu Numeric.py
@@ -0,0 +1,26 @@
+def fac(n):
+    f=1
+    for i in range(1,n+1):
+        f*=i
+    return f
+
+def fib(n):
+    a,b=-1,1
+    for i in range(n):
+        print(a+b,end=',')
+        a,b=b,a+b
+
+while True:
+    m=int(input("1 Factorial of a number \n2 Generate 'N' Fibonacci series \nExit \nChoose your option: "))
+    if m==1:
+        n=int(input('Enter number to find its factorial: '))
+        print(n,'! is ',fac(n))
+        print()
+
+    elif m==2:
+        n=int(input('Enter number to find its fibonacci series: '))
+        fib(n)
+        print()
+
+    elif m==3:
+        break
