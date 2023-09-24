a557cc50bf651982082b0b3e7a8879680d7cf4fe
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-09-24 13:57:25 +0530

    8 Nested Dictionary (finding max).py

--- Record Codes/8 Nested Dictionary (finding max).py
+++ Record Codes/8 Nested Dictionary (finding max).py
@@ -1,12 +1,3 @@
-'''NESTED DICTIONARY
-AIM: To create a nested dictionary and manipulate the same.
-METHODOLOGY: A nested dictionary is created having the main keys to be 3 categories
-namely SENIORS, JUNIORS, SUBJUNIORS. For each of these keys, an inner dictionary is
-created with keys as BHARATHI, TAGORE, SHIVAJI, PRATAP and values as the score for that
-house. The code makes use of an user defined function MAX_SCORE() that takes the
-dictionary as parameter and displays the house having maximum score for each category.
-Note: Strictly follow the uppercase for all the keys as mentioned above.'''
-
 def MAX_SCORE(d):
     for i in d:
         highest = 0
